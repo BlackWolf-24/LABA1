5a30aed9146d559d9adc19b1e73ef4ff02c370e7
Ilya0 <Ilya0@DESKTOP-BA9DOO3>
2022-10-03 13:26:48 +0300

    Добавьте файлы проекта.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,29 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace LABA1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+        private ILoggerManager _logger;
+
+        public WeatherForecastController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Вот информационное сообщение от нашего контроллера значений.");
+            _logger.LogDebug("Вот отладочное сообщение от нашего контроллера значений.");
+            _logger.LogWarn("Вот сообщение предупреждения от нашего контроллера значений.");
+            _logger.LogError("Вот сообщение об ошибке от нашего контроллера значений.");
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
